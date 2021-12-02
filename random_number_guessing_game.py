960d701e4deeaa652cc7e76008f3d2935fb4a197
Michael Haslam <hreikin@gmail.com>
2021-12-02 19:25:53 +0000

    Add basic "3 try" functionality.

--- random_number_guessing_game.py
+++ random_number_guessing_game.py
@@ -9,27 +9,39 @@ seed()
 number = randint(1, 10)
 running = True
 
+current_try = 0
+max_tries = 3
+
+print("A number between 1 and 10 has been picked at random, can you guess which one it is ? You have 3 tries.")
+
 # If the variable "running" is True keep going through the loop
 while running:
-    guess = int(input("Guess a number between 1 and 10: "))
 
-    # If the guess is equal to the number print out the winning statement.
-    if guess == number:
-        print("Congratulations, that is correct!")
+    # Check if current_try is lower than max_tries. If it is then run through the loop, if it isn't then print "Game Over."
+    if current_try < max_tries:
+        guess = int(input("Guess a number between 1 and 10: "))
 
-        # This causes the while loop to stop.
-        running = False
+        # If the guess is equal to the number print out the winning statement.
+        if guess == number:
+          print("Congratulations, that is correct!")
 
-    # If the guess is less than the number print out a statement advising them to guess higher.
-    elif guess < number:
-        print("No, it's a little higher than that.")
+          # This causes the while loop to stop.
+          running = False
 
-    # If neither of the above are true (so they guessed higher than the number) print out a statement advising them to guess lower.
+        # If the guess is less than the number print out a statement advising them to guess higher.
+        elif guess < number:
+            print("No, it's a little higher than that.")
+            current_try = current_try + 1
+
+        # If neither of the above are true (so they guessed higher than the number) print out a statement advising them to guess lower.
+        else:
+            print("No, it's a little lower than that.")
+            current_try = current_try + 1
+
     else:
-        print("No, it's a little lower than that.")
+        print("Game Over.")
+        running = False
 
 # Once the "while" loop has completed print out the finished message.
 else:
-    print("Game completed.")
-
-print("Goodbye.")
+    print("Goodbye.")
