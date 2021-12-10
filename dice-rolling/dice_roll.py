372eb8e3740f1b2fa360a8e49961da8d8fbecb06
Michael Haslam <hreikin@gmail.com>
2021-12-10 14:13:39 +0000

    Lowers the sleep value and adds extra comments to describe the code.

--- dice-rolling/dice_roll.py
+++ dice-rolling/dice_roll.py
@@ -6,6 +6,7 @@ from time import sleep
 # Seed the random number generation. This method uses the users system time.
 seed()
 
+# Set variable default values.
 roll_again = True
 number_of_dice = 0
 current_dice_roll = 1
@@ -15,8 +16,10 @@ while roll_again:
     number_of_dice = int(input("How many dice do you want to roll ? "))
     print("Ok, let's roll the dice.")
     print("Rolling...")
-    sleep(1)
+    sleep(0.5)
 
+    # Prints out the dice roll result(s) until the amount (number_of_dice) selected by the user is reached. 
+    # When finished it resets the "current_dice_roll" back to its default value.
     while current_dice_roll <= number_of_dice:
         print("Dice number", current_dice_roll, "is a value of:", randint(1, 6))
         current_dice_roll += 1
@@ -25,6 +28,7 @@ while roll_again:
 
     # Ask if the user wants to roll again.
     roll_again = input("Do you want to roll again (yes/no) ? ")
+    
     # Check the users answer and either restart the loop or end it.
     if roll_again.lower().startswith("y") or roll_again.upper().startswith("Y"):
             print("Ok, let's carry on then.")
