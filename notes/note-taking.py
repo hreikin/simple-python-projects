e052d8e7aeddd8073fc6a204ae91d63a2ca43f3c
Michael Haslam <hreikin@gmail.com>
2021-12-11 10:09:18 +0000

    Add "file_extension" variable and include it where needed.This sets the default file extension to ".txt".

--- notes/note-taking.py
+++ notes/note-taking.py
@@ -1,6 +1,8 @@
 import os
 import platform
 
+file_extension = ".txt"
+
 # Check which OS the user has and figure out and join the home drive, path and "save_location" 
 # for windows, or just home directory and "save_location" for linux/mac. We use "os.path.join" 
 # instead of joining strings to ensure the path has the correct format expected by the users 
@@ -20,7 +22,7 @@ print("\n***Welcome to the note taking app!***\n")
 print(f"The current location files will be saved is \"{save_location}\".\n")
 
 # Ask the user for a "file_name".
-file_name = input("Please give a name and file extension to the note you want to save: ")
+file_name = input("Please give a name to the note you want to save: ")
 
 # Ask the user for the "file_content".
 file_content = input("Please write your note now and hit ENTER to save: ")
@@ -31,18 +33,18 @@ def open_directory(path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     return open(path, "w")
 
-with open_directory(save_location+file_name) as file:
+with open_directory(save_location + file_name + file_extension) as file:
 
     file.write(file_content)
     file.close()
 
 # Show the saved files location and "file_content".
-print(f"The file was saved to: \"{save_location}/{file_name}\"")
+print(f"The file was saved to: \"{save_location}/{file_name}{file_extension}\"")
 # print(f"The files content was:\n\n{file_content}")
 
 print("The files content was:\n")
 
-file = open(save_location+file_name)
+file = open(save_location + file_name + file_extension)
 
 while True:
     line = file.readline()
