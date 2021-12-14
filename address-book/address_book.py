c731c63d97af7a17f3ee6500ba9925563637e376
Michael Haslam <hreikin@gmail.com>
2021-12-14 12:37:12 +0000

    File created with basic description of task/project to be completed.

---
+++ address-book/address_book.py
@@ -0,0 +1,11 @@
+##################################### TASK ######################################
+# Create your own command-line address-book program which you can browse, add, 
+# modify, delete or search through for your contacts such as friends, family and 
+# colleagues and their information such as email address and/or phone number. 
+# Details must be stored for later retrieval.
+
+##################################### HINT ######################################
+# Create a class to represent the person's information. Use a dictionary to store 
+# person objects with their name as the key. Use the pickle module to store the 
+# objects persistently on your hard disk. Use the dictionary built-in methods to 
+# add, delete and modify the persons.
