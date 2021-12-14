b552e216261027f0bf890937d176eac65cc86192
Michael Haslam <hreikin@gmail.com>
2021-12-14 21:37:26 +0000

    Add comments explaining the problems being solved.

--- automate-the-boring-stuff/comma-code.py
+++ automate-the-boring-stuff/comma-code.py
@@ -1,3 +1,9 @@
+# Write a function that takes a list value as an argument and returns
+# a string with all the items separated by a comma and a space, with and
+# inserted before the last item. For example, passing the previous spam list to
+# the function would return 'apples, bananas, tofu, and cats' . But your func-
+# tion should be able to work with any list value passed to it.
+
 spam = ["apples", "bananas", "tofu", "cats"]
 
 def insert_string(list_to_print):
