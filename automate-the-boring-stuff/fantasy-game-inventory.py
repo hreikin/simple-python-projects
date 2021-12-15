7d70a56f8823d78fe6fcb793e11d9352d66e10d6
Michael Haslam <hreikin@gmail.com>
2021-12-15 14:30:09 +0000

    Completed List To Dictionary Fucntion For Fantasy Game Inventory Practice Project:
    Imagine that a vanquished dragon’s loot is represented as a list of strings
    like this:
    dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
    Write a function named addToInventory(inventory, addedItems) , where the
    inventory parameter is a dictionary representing the player’s inventory (like
    in the previous project) and the addedItems parameter is a list like dragonLoot .
    The addToInventory() function should return a dictionary that represents the
    updated inventory. Note that the addedItems list can contain multiples of the
    same item.

--- automate-the-boring-stuff/fantasy-game-inventory.py
+++ automate-the-boring-stuff/fantasy-game-inventory.py
@@ -23,3 +23,27 @@ def displayInventory(inventory):
     print("Total number of items: " + str(item_total))
 
 displayInventory(stuff)
+
+# Imagine that a vanquished dragon’s loot is represented as a list of strings
+# like this:
+# dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+# Write a function named addToInventory(inventory, addedItems) , where the
+# inventory parameter is a dictionary representing the player’s inventory (like
+# in the previous project) and the addedItems parameter is a list like dragonLoot .
+# The addToInventory() function should return a dictionary that represents the
+# updated inventory. Note that the addedItems list can contain multiples of the
+# same item.
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
