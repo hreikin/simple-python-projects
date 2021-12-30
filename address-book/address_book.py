8fa21ea76460d2176d8820169e07dd058a3f964b
Michael Haslam <hreikin@gmail.com>
2021-12-30 16:41:04 +0000

    Revert docstring work.
    This reverts commit cd92d9e4b5279e7ab24b032bba37b1bc2f57c48d.

--- address-book/address_book.py
+++ address-book/address_book.py
@@ -13,334 +13,195 @@ from pathlib import Path
 import pickle
 
 class Person(object):
-	"""Creates a Person object to store the contact details in.
-
-	Creates a Person object to store the contact details in. This is then stored
-	within a dictionary to create an address book via nesting like so:
-
-				{
-				 Example Name : {
-								  name:"Example Name",
-								  email:"example@example.com",
-								  phone:"12345678910",
-								  address:"123 Example Street, Example Town"
-								 },
-				 Another Name : {
-								  name:"Another Name",
-								  email:"another@example.com",
-								  phone:"10987654321",
-								  address:"123 Another Street, Another Town"
-								 },
-				}
-	"""
-
-	def __init__(self, name = "", email = "", phone = "", address = ""):
-		"""Initializes all the variables.
-
-		Initializes all the variables with empty defaults that are required for
-		storing the persons contact details. These are:
-
-		name
-		email
-		phone
-		address
-		"""
-
-		self.name = name
-		self.email = email
-		self.phone = phone
-		self.address = address
+    def __init__(self, name = "", email = "", phone = "", address = ""):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
 
 class AddressBookApp(object):
-	"""Holds all the functions required to run the address book."""
+    def __init__(self):
+        self.save_location = Path("./address_book.data", exist_ok=True)
+        self.address_book = {}
+        if self.save_location.exists():
+            with open(self.save_location, "rb") as savefile:
+                self.address_book = pickle.load(savefile)
+        else:
+            self.address_book = {}
 
-	def __init__(self):
-		"""Initializes the save_location and loads the address_book variables.
+    def save_details(self):
+        with open(self.save_location, "wb") as savefile:
+            pickle.dump(self.address_book, savefile)
 
-		Initializes the save_location and loads the address_book variables. If
-		the save_location already exists then it loads the data from a file,
-		otherwise it sets the address_book variable to an empty default
-		dictionary value.
-		"""
-		self.save_location = Path("./address_book.data", exist_ok=True)
-		if self.save_location.exists():
-			with open(self.save_location, "rb") as savefile:
-				self.address_book = pickle.load(savefile)
-		else:
-			self.address_book = {}
+    def print_line(self, **info):
+        print(f"{info['name']:<25} {info['email']:<25} {info['phone']:<25} {info['address']:<25}")
 
-	def save_details(self):
-		"""Uses the pickle module to save the address_book variable to file."""
-		with open(self.save_location, "wb") as savefile:
-			pickle.dump(self.address_book, savefile)
+    def view_all(self):
+        if not self.address_book:
+            print("No contacts found.")
+            return
+        self.print_line(name="Name", email="Email", phone="Phone", address="Address")
+        for info in self.address_book.values():
+            self.print_line(**vars(info))
+        input("\nPress ENTER to return to the menu.")
 
-	def print_line(self, **info):
-		"""Prints the headings of the view_all table.
+    def get_details(self):
+        while True:
+            name = input("Name: ").title()
+            if name in self.address_book:
+                print("A contact is already present with that name.")
+                pass
+            else:
+                email = input("Email: ")
+                phone = input("Phone Number: ")
+                address = input("Address: ").title()
+                return name, email, phone, address
+                
+    def add_contact(self):
+        while True:
+            print("Ok, let's add a new contact. Please provide the following info.")
+            name, email, phone, address = self.get_details()
+            if self.ask_question(None) == True:
+                self.address_book[name] = Person(name, email, phone, address)
+                self.save_details()
+                print("Contact added successfully.")
+            # self.retry_question()
+            if self.ask_question() == False:
+                return
 
-		Prints the headings of the view_all table with each one having a max
-		length of 25 characters.
-		"""
-		print(f"{info['name']:<25} {info['email']:<25} {info['phone']:<25} {info['address']:<25}")
+    def edit_contact(self):
+        while True:
+            print("Ok, let's edit a contact. Please provide the contacts name.")
+            old_name = input("Name: ").title()
+            if old_name not in self.address_book:
+                    print("There is no contact by that name.")
+                    pass
+            else:
+                print("Input the new information below.")
+                name, email, phone, address = self.get_details()
+                if self.ask_question(None) == True:
+                    self.address_book.pop(old_name)
+                    self.address_book[name] = Person(name, email, phone, address)
+                    self.save_details()
+                    print("Contact successfully updated.")
+            # self.retry_question()
+            if self.ask_question() == False:
+                return
 
-	def view_all(self):
-		"""Checks if the address_book contains any entries and displays them.
+    def delete_contact(self):
+        while True:
+            name = input("What is the name of the contact you wish to delete: ").title()
+            confirm_delete = f"Are you sure you want to delete the info for {name.title()} (yes/no) ? "
+            if name not in self.address_book:
+                print("There is no contact by that name.")
+                pass
+            elif self.ask_question(confirm_delete) == True:
+                self.address_book.pop(name)
+                print(f"Ok, {name} is deleted.")
+                self.save_details()
+            # self.retry_question()
+            if self.ask_question() == False:
+                return
+    
+    def search_contact(self):
+        while True:
+            search_name = input("What is the name of the contact you wish to search for: ").title()
+            if search_name not in self.address_book:
+                print("There is no contact by that name.")
+                pass
+            else:
+                search_item = self.address_book[search_name]
+                for key, value in vars(search_item).items():
+                    print(key + ":", value)
+            if self.ask_question() == False:
+                return
 
-		Checks if the address_book contains any entries and displays them. If
-		no entries are found it displays a message notifying the user. If any
-		entries are found it calls the print_line function and displays the
-		entries below the headings.
-		"""
-		if not self.address_book:
-			print("No contacts found.")
-			return
-		self.print_line(name="Name", email="Email", phone="Phone", address="Address")
-		for info in self.address_book.values():
-			self.print_line(**vars(info))
-		input("\nPress ENTER to return to the menu.")
+    def ask_question(self, question="Do you want to do this action again (yes/no) ? "):
+        CONFIRM = "Are you sure you want to do this (yes/no) ? "
+        ask = question or CONFIRM
+        val = input(ask).lower()
+        if val.lower() in "yes":
+            return True
+        if val.lower() in "no":
+            print("OK, cancelling.")
+            return False
+        # Invalid input response.
+        self.invalid_input()
+        return self.ask_question(question)
 
-	def get_details(self):
-		"""Asks the user to input the contact details and returns the variables.
+    def retry_question(self):
+        ask_to_retry = self.ask_question()
+        return ask_to_retry
 
-		Asks the user to input the contacts details. This function is used by
-		the add_contact and edit_contact functions to collect the contact
-		information from the user.
+    def print_options(self, options):
+        """Prints something like this for a dict of options:
 
-		The function checks to ensure another contact with the same name doesn't
-		already exist within the address_book before continuing to ask for the
-		remaining information.
-		"""
-		while True:
-			name = input("Name: ").title()
-			if name in self.address_book:
-				print("A contact is already present with that name.")
-				pass
-			else:
-				email = input("Email: ")
-				phone = input("Phone Number: ")
-				address = input("Address: ").title()
-				return name, email, phone, address
+        e.g. from complex options:
 
-	def add_contact(self):
-		"""Asks for the contact details and creates a Person object with the data.
+            1. Create a note.
+            2. Edit a note.
+            3. Delete a note.
+            4. List all notes.
+            5. Exit/Quit
 
-		Asks for the contact details and creates a Person object with the data.
-		This function calls the get_details function to gather the information
-		and then calls the ask_question function with the argument set to None
-		so it can confirm this action is what the user wants to complete.
+        example:
 
-		It then adds the created Person object as an entry of the address_book
-		dictionary with the information supplied for the name as its key.
-
-		Once this is complete it finally calls the ask_question function using
-		the default argument to check if the user wants to add another contact or
-		return to the menu.
-		"""
-		while True:
-			print("Ok, let's add a new contact. Please provide the following info.")
-			name, email, phone, address = self.get_details()
-			if self.ask_question(None) == True:
-				self.address_book[name] = Person(name, email, phone, address)
-				self.save_details()
-				print("Contact added successfully.")
-			# self.retry_question()
-			if self.ask_question() == False:
-				return
-
-	def edit_contact(self):
-		"""Asks for a name, checks it exists, then edits the existing details.
-		
-		Asks for a name, checks it exists, then edits the existing details. If 
-		the name doesnt exist it asks for another, if it does exist it calls the
-		get_details function to collect the new information. 
-		
-		Once this is complete it calls the ask_question function with the 
-		argument set to None to confirm the user wants to change the details. 
-		Once confirmed it uses .pop to remove the old details and inserts the 
-		new ones before it calls the save_details function to save them.
-		
-		Finally it calls the ask_question function with using the default 
-		argument to check if the user wants to do this action again or return to 
-		the menu.
-		"""
-		while True:
-			print("Ok, let's edit a contact. Please provide the contacts name.")
-			old_name = input("Name: ").title()
-			if old_name not in self.address_book:
-					print("There is no contact by that name.")
-					pass
-			else:
-				print("Input the new information below.")
-				name, email, phone, address = self.get_details()
-				if self.ask_question(None) == True:
-					self.address_book.pop(old_name)
-					self.address_book[name] = Person(name, email, phone, address)
-					self.save_details()
-					print("Contact successfully updated.")
-			# self.retry_question()
-			if self.ask_question() == False:
-				return
-
-	def delete_contact(self):
-		"""Asks for a name and removes the contact from the address book.
-		
-		Asks for a name and removes the contact from the address_book. If the 
-		name does not exist it prints a message to the user. If the name does 
-		exist it calls the ask_question function with the message specified in 
-		the confirm_delete variable to confirm the user wants to complete this 
-		action.
-		
-		Once confirmed it uses .pop to remove the item from the address_book and 
-		then calls the save_details function to save the new state of the 
-		address_book.
-		
-		Once this is complete it finally calls the ask_question function to see
-		if the user wants to do this action again or return to the menu.
-		"""
-		while True:
-			name = input("What is the name of the contact you wish to delete: ").title()
-			confirm_delete = f"Are you sure you want to delete the info for {name.title()} (yes/no) ? "
-			if name not in self.address_book:
-				print("There is no contact by that name.")
-				pass
-			elif self.ask_question(confirm_delete) == True:
-				self.address_book.pop(name)
-				print(f"Ok, {name} is deleted.")
-				self.save_details()
-			# self.retry_question()
-			if self.ask_question() == False:
-				return
-
-	def search_contact(self):
-		"""Asks for a name and if it exists prints out the details.
-		
-		Asks for a name and if it exists prints out the details. If it doesn't 
-		exist a message is printed to the user. Once the details have been shown 
-		to the user it then calls the ask_question function with the default 
-		argument to check if the user wants to do this action again.
-		"""
-		while True:
-			search_name = input("What is the name of the contact you wish to search for: ").title()
-			if search_name not in self.address_book:
-				print("There is no contact by that name.")
-				pass
-			else:
-				search_item = self.address_book[search_name]
-				for key, value in vars(search_item).items():
-					print(key + ":", value)
-			if self.ask_question() == False:
-				return
-
-	def ask_question(self, question="Do you want to do this action again (yes/no) ? "):
-		"""Either confirms or repeats the last action. Returns True or False.
-		
-		Either confirms or repeats the last action. Returns True or False. The 
-		default question asks if the user wants to do the last action again. The 
-		CONFIRM variable allows a second option for confirming if the user wants 
-		to continue. An argument can also be provided via a variable when the 
-		function is called to provide another question that requires a yes or no 
-		answer.
-		"""
-		CONFIRM = "Are you sure you want to do this (yes/no) ? "
-		ask = question or CONFIRM
-		val = input(ask).lower()
-		if val.lower() in "yes":
-			return True
-		if val.lower() in "no":
-			print("OK, cancelling.")
-			return False
-		# Invalid input response.
-		self.invalid_input()
-		return self.ask_question(question)
-
-	# def retry_question(self):
-		# ask_to_retry = self.ask_question()
-		# return ask_to_retry
-
-	def print_options(self, options):
-		"""Prints something like this for a dict of options:
-
-		e.g. from complex options:
-
-			1. Create a note.
-			2. Edit a note.
-			3. Delete a note.
-			4. List all notes.
-			5. Exit/Quit
-
-		example:
-
-			>>> print_options({ 'key': {'title': 'apples', 'other': {'title': 'egg'} }})
-			1. apples
-			2. egg
+            >>> print_options({ 'key': {'title': 'apples', 'other': {'title': 'egg'} }})
+            1. apples
+            2. egg
 
 
-		"""
-		for index, (key, sub_dict) in enumerate(options.items()):
-			row = f"{index+1}. {sub_dict['title']}"
-			print(row)
+        """
+        for index, (key, sub_dict) in enumerate(options.items()):
+            row = f"{index+1}. {sub_dict['title']}"
+            print(row)
 
-	def main_loop(self, options):
-		"""Prints the menu to the screen and allows selection of functions.
-		
-		Prints the menu to the screen and allows selection of functions. This 
-		calls the print_options function with the options argument defined in 
-		the main function to display the menu to the user. Once a selection has 
-		been made the relevant function is run. If an invalid charcter is 
-		detected as input it prints a message to the user.
-		"""
-		running = True
-		while running:
+    def main_loop(self, options):
+        running = True
+        while running:
 
-			print("\nWelcome to the address book app.")
-			self.print_options(options)
+            print("\nWelcome to the address book app.")
+            self.print_options(options)
 
-			q = "Type in a number to make a selection: "
-			int_current_selection = self.ask_number(q)
+            q = "Type in a number to make a selection: "
+            int_current_selection = self.ask_number(q)
 
-			choices = tuple(options.values())
-			# Takes the users input and runs the appropriate function. If the input is invalid it asks
-			# the user to try again.
-			try:
-				selection = choices[int_current_selection-1]
-			except IndexError:
-				selection = {
-					'title': "Option does not exist.",
-					'func': self.invalid_input,
-				}
+            choices = tuple(options.values())
+            # Takes the users input and runs the appropriate function. If the input is invalid it asks
+            # the user to try again.
+            try:
+                selection = choices[int_current_selection-1]
+            except IndexError:
+                selection = {
+                    'title': "Option does not exist.",
+                    'func': self.invalid_input,
+                }
 
-			print(selection['title'])
-			#run our found function
-			func = selection['func']
-			running = func()
-			if running is None:
-				running = True
+            print(selection['title'])
+            #run our found function
+            func = selection['func']
+            running = func()
+            if running is None:
+                running = True
 
-	def ask_number(self, q=None):
-		"""Takes user input for selection and raises an exception if invalid.
-		
-		Takes user input for selection and raises an exception if invalid. 
-		Converts the string input to an int value and raises an which then calls 
-		the invalid_input function if the string cannot be converted to an int.
-		"""
-		default = "Type in a number to make a selection: "
-		try:
-			val = input(q or default)
-			int_val = int(val)
-		except ValueError as e:
-			self.invalid_input()
-			return self.ask_number(q)
+    def ask_number(self, q=None):
+        default = "Type in a number to make a selection: "
+        try:
+            val = input(q or default)
+            int_val = int(val)
+        except ValueError as e:
+            self.invalid_input()
+            return self.ask_number(q)
 
-		return int_val
+        return int_val
 
-	def invalid_input(self):
-		"""Prints a message explaining the input is invalid."""
-		default = "Invalid input, please try again."
-		print(default)
+    def invalid_input(self):
+        default = "Invalid input, please try again."
+        print(default)
 
-	def close_statement(self):
-		"""Prints a message before returning False to exit the program."""
-		print("Ok, exiting program now.")
-		return False
+    def close_statement(self):
+        print("Ok, exiting program now.")
+        return False
 
 def main():
 	"""Instantiates/runs the AddressBookApp and defines a menu via a dictionary.
