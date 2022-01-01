0b60ed04cd377c70e840f8a52908aa493120a129
Michael Haslam <hreikin@gmail.com>
2022-01-01 19:20:21 +0000

    Refactor the check_events() and update_screen() functions into their own module to simplify the structure of the code in the main alien_invasion.py file.

--- python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
+++ python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
@@ -1,9 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize pygame, settings and the screen object.
@@ -18,16 +17,11 @@ def run_game():
     # Start the main loop for the game.
     while True:
 
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        # Call check_events function to watch for keyboard and mouse events.
+        gf.check_events()
 
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_colour)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        # Call update_screen function to update the images and flip to the new 
+        # screen.
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
