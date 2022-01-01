c1d97f04257735883e258a042d3742e4f918cc5b
Michael Haslam <hreikin@gmail.com>
2022-01-01 22:47:46 +0000

    Refactor the check_events function. Create two new functions to deal with key press and key release events separately.

--- python-crash-course-hands-on-projects/alien-invasion/game_functions.py
+++ python-crash-course-hands-on-projects/alien-invasion/game_functions.py
@@ -2,25 +2,31 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+    """Respond to key press events."""
+    # Move the ship to the right or left.
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Respond to key release events."""
+    # Stop moving when key is released.
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Respond to key presses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
-        # Move the ship to the right or left.
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
-        # Stop moving when key is released.
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """Update the images on the screen and flip to the new screen."""
