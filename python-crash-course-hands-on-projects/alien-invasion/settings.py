955fc16c39c537feb2d194bc8a46f77a6cd6ea59
Michael Haslam <hreikin@gmail.com>
2022-01-01 18:48:40 +0000

    Change bg_colour to match the background of the bitmap image used for the player ship.

--- python-crash-course-hands-on-projects/alien-invasion/settings.py
+++ python-crash-course-hands-on-projects/alien-invasion/settings.py
@@ -7,4 +7,4 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         # Set the background colour to grey.
-        self.bg_colour = (50, 50, 50)
+        self.bg_colour = (230, 230, 230)
