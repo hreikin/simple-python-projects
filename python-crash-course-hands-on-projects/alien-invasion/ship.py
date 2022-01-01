d8b79d0e59a60186092de51b62bf6eb4afc770b0
Michael Haslam <hreikin@gmail.com>
2022-01-01 18:50:28 +0000

    Create the Ship() class to handle most of the player ships behaviour.

---
+++ python-crash-course-hands-on-projects/alien-invasion/ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ship():
+    """A class to manage most of the player ships behaviour."""
+
+    def __init__(self, screen):
+        """Initialize the ship and set its starting position."""
+        self.screen = screen
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new ship in the centre at the bottom of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
