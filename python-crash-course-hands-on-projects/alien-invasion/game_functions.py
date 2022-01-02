cfe88da105ef8b2efaf407306ba6ddece1062e68
Michael Haslam <hreikin@gmail.com>
2022-01-02 10:09:43 +0000

    Listen for key press event and fire bullets when spacebar is pressed. Updates the screen to keep the bullets behind the ship and aliens.

--- python-crash-course-hands-on-projects/alien-invasion/game_functions.py
+++ python-crash-course-hands-on-projects/alien-invasion/game_functions.py
@@ -2,13 +2,20 @@ import sys
 
 import pygame
 
-def check_keydown_events(event, ship):
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to key press events."""
     # Move the ship to the right or left.
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    # Fire a bullet when spacebar is pressed.
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Respond to key release events."""
@@ -18,20 +25,24 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to key presses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update the images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_colour)
+
+    # Redraw all bullets behind the ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # Make the most recently drawn screen visible.
