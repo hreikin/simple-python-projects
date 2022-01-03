1560eb2b880ba23e78878da5a7c46d12ec853996
Michael Haslam <hreikin@gmail.com>
2022-01-03 13:32:43 +0000

    Refactor some of the run_game method in to two extra methods to handle keyboard and mouse events and updating the screen.

--- python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
+++ python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
@@ -25,18 +25,23 @@ class AlienInvasion():
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop, set the 
-            # background colour and draw the ship.
-            self.screen.fill(self.settings.bg_colour)
-            self.ship.blitme()
+    def _check_events(self):
+        # Respond to keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+        # Redraw the screen, set the background colour and draw the ship.
+        self.screen.fill(self.settings.bg_colour)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # Make an instance of the class and run the game.
