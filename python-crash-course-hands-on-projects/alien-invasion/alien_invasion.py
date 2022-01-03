816f111ab8a4cee5aaa78b950542f29ecbb32d95
Michael Haslam <hreikin@gmail.com>
2022-01-03 13:27:01 +0000

    Create a Ship class to hold all the player ships settings and integrate the changes with the main alien_invasion.py file.

--- python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
+++ python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion():
     """Overall class to manage game assets and behaviour."""
@@ -18,6 +19,9 @@ class AlienInvasion():
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # Instantiate the player ship.
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -26,9 +30,10 @@ class AlienInvasion():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Redraw the screen during each pass through the loop and set the 
-            # background colour from settings.py.
+            # Redraw the screen during each pass through the loop, set the 
+            # background colour and draw the ship.
             self.screen.fill(self.settings.bg_colour)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
