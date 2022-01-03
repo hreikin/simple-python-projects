d6355bbe88b52eda694ac643821b13d681aa9f46
Michael Haslam <hreikin@gmail.com>
2022-01-03 15:10:51 +0000

    Creates a Bullet class to manage the bullets fired from the ship.

---
+++ python-crash-course-hands-on-projects/alien-invasion/bullet.py
@@ -0,0 +1,33 @@
+from typing import Sized
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__ (self, ai_game):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.colour = self.settings.bullet_colour
+
+        # Create a bullet rect at (0, 0) and then correct the position.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Store the bullet's position as a decimal value.
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+
+        # Update the bullet rects position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.colour, self.rect)
