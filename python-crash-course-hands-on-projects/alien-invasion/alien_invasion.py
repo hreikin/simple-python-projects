b345a26fbb42419656eee348dec33a58262548e3
Michael Haslam <hreikin@gmail.com>
2022-01-03 12:55:02 +0000

    Background colour set to an off white to match the background of the ship bitmap image.

--- python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
+++ python-crash-course-hands-on-projects/alien-invasion/alien_invasion.py
@@ -12,6 +12,9 @@ class AlienInvasion():
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background colour.
+        self.bg_colour = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -19,6 +22,9 @@ class AlienInvasion():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_colour)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
