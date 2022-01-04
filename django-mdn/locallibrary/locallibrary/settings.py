b2d870c6b651c6c390ad5652cea75ecc96471249
Michael Haslam <hreikin@gmail.com>
2022-01-04 15:54:33 +0000

    Set the timezone and register the catalog app as an installed application.

--- django-mdn/locallibrary/locallibrary/settings.py
+++ django-mdn/locallibrary/locallibrary/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add new application. This object was created for us in "/catalog/apps.py".
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
