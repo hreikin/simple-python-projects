32949069e4dad2f9f364de5c0d489726a5adc5bf
Michael Haslam <hreikin@gmail.com>
2022-01-04 14:09:40 +0000

    Move the development secret key to a .env file.

--- django-mdn/locallibrary/locallibrary/settings.py
+++ django-mdn/locallibrary/locallibrary/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3i0po9a&u4ep2@*qth$fscj3_@xsrppxf-gna7)-@v)!6rt2t5'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
