74c1096963c2632214fd2ef9e8ddb58214fe1b90
Michael Haslam <hreikin@gmail.com>
2022-01-04 14:10:50 +0000

    Register the catalog application as an installed app.

--- django-mdn/locallibrary/locallibrary/settings.py
+++ django-mdn/locallibrary/locallibrary/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add new app. This object was created for us in "/catalog/apps.py".
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
