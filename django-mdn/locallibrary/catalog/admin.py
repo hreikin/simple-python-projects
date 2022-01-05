ae2380c39c84afbe326cc6c314428fd2f5566c20
Michael Haslam <hreikin@gmail.com>
2022-01-05 19:29:00 +0000

    Register created models in admin site.

--- django-mdn/locallibrary/catalog/admin.py
+++ django-mdn/locallibrary/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# Import created models.
+from .models import Author, Book, BookInstance, Genre, Language
 
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
