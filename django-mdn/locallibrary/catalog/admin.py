6e0268e378b198f259b2f4b93b775260f3e9b198
Michael Haslam <hreikin@gmail.com>
2022-01-05 21:49:37 +0000

    Register ModelAdmin classes for selected models.

--- django-mdn/locallibrary/catalog/admin.py
+++ django-mdn/locallibrary/catalog/admin.py
@@ -4,8 +4,26 @@ from .models import Author, Book, BookInstance, Genre, Language
 
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+# Define the admin class.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model.
+# admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator, this does the same as 
+# the admin.site.register() syntax
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator.
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Genre)
 admin.site.register(Language)
