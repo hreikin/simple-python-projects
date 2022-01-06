a4a7fe0e682c6fcbb53986a589e8aab55d96f651
Michael Haslam <hreikin@gmail.com>
2022-01-06 15:05:29 +0000

    Fix missing underscore.

--- django-mdn/locallibrary/catalog/views.py
+++ django-mdn/locallibrary/catalog/views.py
@@ -12,7 +12,7 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a').
-    num_instances_available = BookInstance.objects.filter(status_exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
