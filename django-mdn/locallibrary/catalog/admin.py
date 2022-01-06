7face4f997c47bb962c2fb4ab97447924f0b150d
Michael Haslam <hreikin@gmail.com>
2022-01-06 11:13:17 +0000

    Set fieldset values to group related fields when viewing the detail view for models.

--- django-mdn/locallibrary/catalog/admin.py
+++ django-mdn/locallibrary/catalog/admin.py
@@ -6,26 +6,50 @@ from .models import Author, Book, BookInstance, Genre, Language
 
 # Define the admin class.
 class AuthorAdmin(admin.ModelAdmin):
+    # Defines which fields are shown when viewing the list of authors in the 
+    # admin site.
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
+
+    # Defines how to show the fields inside the admin interface when viewing an 
+    # author. When grouped in brackets they will show on the same row. Fieldsets 
+    # can be used instead of fields to group the information together.
+    # 
+    # fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    fieldsets = (
+        ('Details', {
+            'fields': ('last_name', 'first_name', ('date_of_birth', 'date_of_death'))
+        }),
+    )
+
 # Register the admin class with the associated model.
-# admin.site.register(Author)
 admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator, this does the same as 
 # the admin.site.register() syntax
-# admin.site.register(Book)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'genre')
     list_display = ('title', 'author', 'display_genre')
+    fieldsets = (
+        ('Details', {
+            'fields': ('title', 'author', 'summary', 'genre', 'isbn', 'language')
+        }),
+    )
 
 # Register the Admin classes for BookInstance using the decorator.
-# admin.site.register(BookInstance)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
     list_display = ('book', 'status', 'due_back')
+    fieldsets = (
+        ('Details', {
+            'fields': ('id', 'book', 'imprint')
+        }),
+        ('Availability', {
+            'fields': ('due_back', 'status')
+        }),
+    )
 
 admin.site.register(Genre)
 admin.site.register(Language)
