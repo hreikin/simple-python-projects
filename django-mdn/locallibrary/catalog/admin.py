31d56e1be1b0349eaeed550f9504573dd343e589
Michael Haslam <hreikin@gmail.com>
2022-01-07 09:41:16 +0000

    Add borrower to BookInstanceAdmin so the fields are available in the admin panel.

--- django-mdn/locallibrary/catalog/admin.py
+++ django-mdn/locallibrary/catalog/admin.py
@@ -57,13 +57,13 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-    list_display = ('book', 'status', 'due_back', 'id')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     fieldsets = (
         ('Details', {
             'fields': ('id', 'book', 'imprint')
         }),
         ('Availability', {
-            'fields': ('due_back', 'status')
+            'fields': ('due_back', 'status', 'borrower')
         }),
     )
 
