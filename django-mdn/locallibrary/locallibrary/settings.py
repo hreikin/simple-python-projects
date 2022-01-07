3f4d0326e4310f4a7a9b01e4bae1d188234a43a7
Michael Haslam <hreikin@gmail.com>
2022-01-07 09:22:09 +0000

    Redirect logged in user to root URL and enable email logging to console for testing.

--- django-mdn/locallibrary/locallibrary/settings.py
+++ django-mdn/locallibrary/locallibrary/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# For testing log emails to the console.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
