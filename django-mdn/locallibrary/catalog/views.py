22773d8c1b285a741dc542b0447f0fa5ca25f71a
Michael Haslam <hreikin@gmail.com>
2022-01-07 10:22:06 +0000

    Create LoanedBooksByUserListView and URL mapping to display loaned book status to logged in users.

--- django-mdn/locallibrary/catalog/views.py
+++ django-mdn/locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -58,3 +59,13 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class based view that lists books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
