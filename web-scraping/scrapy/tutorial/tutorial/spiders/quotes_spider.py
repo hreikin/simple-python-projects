955c48e0edad798761e28b657dd1f87a1b24b395
Michael Haslam <hreikin@gmail.com>
2022-01-08 09:57:50 +0000

    Use start_urls variable as a shortcut to creating requests by using the default implementation of start_requests().

--- web-scraping/scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ web-scraping/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -2,14 +2,10 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
     def parse(self, response):
         page = response.url.split("/")[-2]
