d06748f9ff6e7cc6eaee37115cda058d3b476c58
Michael Haslam <hreikin@gmail.com>
2022-01-08 10:51:11 +0000

    Remove second link and create logic for following links in the target page.

--- web-scraping/scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ web-scraping/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -4,7 +4,7 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        # 'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
@@ -14,3 +14,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css("small.author::text").get(),
                 'tags': quote.css("div.tags a.tag::text").getall(),
             }
+        
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
