f2ee2eea2157166df531ece99b2b1f418484ec7f
Michael Haslam <hreikin@gmail.com>
2022-01-08 13:38:03 +0000

    Tag and Author spiders created to demonstrate link following and data extraction.

---
+++ web-scraping/scrapy/tutorial/tutorial/spiders/author_spider.py
@@ -0,0 +1,26 @@
+# This spider will start from the main page, it will follow all the links to the 
+# authors pages calling the parse_author callback for each of them, and also the 
+# pagination links with the parse callback as we saw before.
+
+import scrapy
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        author_page_links = response.css('.author + a')
+        yield from response.follow_all(author_page_links, self.parse_author)
+
+        pagination_links = response.css('li.next a')
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
