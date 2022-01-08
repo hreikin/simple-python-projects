9a312d8a39a6b02b1b2007b1a6d65c454e4022b2
Michael Haslam <hreikin@gmail.com>
2022-01-08 10:38:26 +0000

    Select data to extract using selectors and yield the result.

--- web-scraping/scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ web-scraping/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -8,8 +8,9 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        with open(filename, "wb") as stream:
-            stream.write(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").get(),
+                'author': quote.css("small.author::text").get(),
+                'tags': quote.css("div.tags a.tag::text").getall(),
+            }
